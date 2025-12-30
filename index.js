7bb4877b3d067aa1ef550252ef55803a3bd5707b
jamied <hey@isuckat.dev>
2025-12-30 03:54:28 -0500

    added titlebar

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { Vector2 } from "./types.js";
 import { Window } from "./window.js";
 
-var win = new Window(new Vector2(100, 10), new Vector2(200,100), `<p style="background: red;">hi</p>`);
+var win = new Window(new Vector2(100, 10), new Vector2(300,200), `<p style="background: red;">hi</p>`);
--- window.js
+++ window.js
@@ -1,19 +1,30 @@
-
-
 export class Window {
-    constructor(position, size, content) {
-        this.position = position;
-        this.size = size;
+    constructor(initPosition, initSize, content) {
+        this.position = initPosition;
+        this.size = initSize;
         this.content = content;
 
         this.element = document.createElement("div");
-        this.element.innerHTML = content;
         this.element.style.position = "fixed";
-        this.element.style.width = `${size.x}px`;
-        this.element.style.height = `${size.y}px`;
-        this.element.style.left = `${position.x}px`;
-        this.element.style.top = `${position.y}px`;
-        this.element.style.backgroundColor = "white";
+        this.element.style.width = `${this.size.x}px`;
+        this.element.style.height = `${this.size.y}px`;
+        this.element.style.left = `${this.position.x}px`;
+        this.element.style.top = `${this.position.y}px`;
+        this.element.style.display = "flex";
+        this.element.style.flexDirection = "column";
+
+        var titlebar = document.createElement("div");
+        titlebar.style.height = "20px";
+        titlebar.style.width = "100%";
+        titlebar.style.backgroundColor = "black";
+        this.element.appendChild(titlebar);
+
+        var contentElement = document.createElement("div");
+        contentElement.innerHTML = content;
+        contentElement.style.flex = "1";
+        contentElement.style.width = "100%";
+        contentElement.style.backgroundColor = "white";
+        this.element.appendChild(contentElement);
 
         document.getElementById("main").appendChild(this.element);
     }
