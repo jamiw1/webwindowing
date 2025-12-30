0db473dc0f7e210656ae1ad54406089f25785d12
jamied <hey@isuckat.dev>
2025-12-30 04:17:03 -0500

    window dragging

--- window.js
+++ window.js
@@ -1,3 +1,5 @@
+import { Vector2 } from "./types.js";
+
 export class Window {
     constructor(content, initPosition, initSize, titlebarHeight) {
         this.content = content;
@@ -11,13 +13,16 @@ export class Window {
         this.element.style.flexDirection = "column";
 
         var titlebar = document.createElement("div");
-        titlebar.id = "windowTitlebar"
+        titlebar.id = "window_titlebar"
         titlebar.style.height = `${this.titlebarHeight}px`;
         titlebar.style.width = "100%";
         titlebar.style.backgroundColor = "black";
+        titlebar.onmousedown = () => {this.dragging = true}
+        titlebar.onmouseup = () => {this.dragging = false}
         this.element.appendChild(titlebar);
 
         var contentElement = document.createElement("div");
+        contentElement.id = "window_content"
         contentElement.innerHTML = content;
         contentElement.style.flex = "1";
         contentElement.style.width = "100%";
@@ -27,6 +32,7 @@ export class Window {
         document.getElementById("main").appendChild(this.element);
 
         this.dragging = false;
+        addEventListener("mousemove", this.onMouseMove.bind(this));
     }
     setPosition(position) {
         this.position = position;
@@ -40,6 +46,18 @@ export class Window {
     }
     setContent(content) {
         this.content = content;
-        this.element.innerHTML = content;
+        this.element.getElementById("window_content").innerHTML = this.content;
+    }
+    setTitlebarHeight(height) {
+        this.titlebarHeight = height;
+        this.element.getElementById("window_titlebar").style.height = `${this.titlebarHeight}px`;
+    }
+
+    
+
+    onMouseMove(event) {
+        if (this.dragging) {
+            this.setPosition(new Vector2(event.clientX, event.clientY));
+        }
     }
 }
