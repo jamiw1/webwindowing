01ce342396552907ac7e70a41d0f8d4c280c81f3
jamied <hey@isuckat.dev>
2026-01-01 00:39:12 -0500

    html file windows

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ import { Window } from "./window.js";
 var win = new Window(`<p style="background: red;">hi</p>`, new Vector2(100, 10), new Vector2(300,200), "File Manager", 18);
 var win2 = new Window(`<p style="background: green;">this is another window</p>`, new Vector2(600, 80), new Vector2(350,150), "window 2", 18);
 
+var win3 = new Window(await fetch("testwindow.html").then(r => r.text()), new Vector2(10, 100), new Vector2(400, 400), "Hey there", 18);
+
 win2.setTitle("Mega fail");
