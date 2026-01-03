31278fde3fcbe8afea5c8e11e27bbc4a52cafa12
jamied <hey@isuckat.dev>
2026-01-03 17:46:04 -0500

    remove extra test windows

--- index.js
+++ index.js
@@ -36,10 +36,3 @@ desktopIcons.forEach(element => {
 document.getElementById("desktop_icons").addEventListener('click', () => {
     desktopIcons.forEach(icon => icon.classList.remove('selected'));
 });
-
-var win = new Window(`<p style="background: red;">hi</p>`, "win1", new Vector2(100, 10), new Vector2(300,200), "File Manager", 18);
-var win2 = new Window(`<p style="background: green;">this is another window</p>`, "win2", new Vector2(600, 80), new Vector2(350,150), "window 2", 18);
-
-var win3 = new Window(await getContentFromUri("testwindow.html"), "win3", new Vector2(10, 100), new Vector2(400, 200), "Hey there", 18);
-
-win2.setTitle("Mega fail");
