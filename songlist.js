da42a4d202e574fe7d6b0509c14da5c017be06c7
jamied <hey@isuckat.dev>
2026-01-04 01:08:05 -0500

    new song + added cover + tooltip style changes

--- songlist.js
+++ songlist.js
@@ -2,8 +2,13 @@ const songs = [
     {
         name: "Whiplash",
         artist: "yungatita",
-        img: "",
+        img: "assets/songcovers/yungatita - Whiplash.png",
         content: `I LOVE THIS SONG!!!! it is absolutely my favorite song of all time, i genuinely can't get enough of it. they perform really well live too! they have a lot of other music, absolutely go check them out!!!`
+    }, {
+        name: "Our Murderous Descent",
+        artist: "the scary jokes",
+        img: "assets/songcovers/the scary jokes - Our Murderous Descent.png",
+        content: "i don't know what about this song makes it like this but it is absolutely some form of crack, the sound design is amazing"
     }
 ]
 
